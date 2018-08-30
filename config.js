28e44b6259a6de526138c09051a6fae9a11f36b6
mohelhaj <42706914+mohelhaj@users.noreply.github.com>
2018-08-30 13:54:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.TOKEN = 'NDg0NjkxMTUyNjA4NjkwMTgz.Dmlrag.3ty50ASnzfS3a6V0B8WqMDfgdCo';
 
-exports.PREFIX = '-';
+exports.PREFIX = '!';
 
 exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
